2a0c69ae7c8d07987541966248736b8b1c406c21
zhichao liu <1964639572@qq.com>
2026-04-19 23:02:49 +0800

    add more on the app.py file

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,17 @@ app = Flask(__name__, template_folder="../frontend/template", static_folder="../
 def index():
     return render_template("index.html")
 
+@app.route("/checkin_details")
+def checkin_details():
+    return render_template("checkin_details.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 if __name__ in "__main__":
     app.run(debug=True)
